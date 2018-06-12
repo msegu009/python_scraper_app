72b8abbdba8c183be16178e2acd30382396f6aa7
Michael Segura <msegu009@outlook.com>
2018-06-12 19:29:10 -0400

    added proto- datastructure

--- scraper.py
+++ scraper.py
@@ -7,3 +7,28 @@ url = "https://www.humblebundle.com/books/pocket-primers-books"
 rsp = requests.get(url)
 
 soup = BeautifulSoup(rsp.text, 'html.parser')
+
+
+
+tier_headers = soup.select(".dd-header-headline")
+for tier in tier_headers:
+	print(tier.text.strip())
+
+#we need to snag tier name and prices
+#proto-datastructure below
+tiers = {
+	"tier1":{
+		"price":500,
+		"products":[
+			"name1",
+			"name2"
+		]
+	}
+	"tier2":{
+		"price":700,
+		"products":[
+			"name1",
+			"name2"
+		]
+	}
+}	
